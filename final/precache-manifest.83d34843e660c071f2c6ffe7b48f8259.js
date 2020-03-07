1156015ed532628e4e8bfa0dd1a9c5ee5b6688a6
Seth <sethp@udel.edu>
2020-03-06 22:08:48 -0500

    Updates

---
+++ final/precache-manifest.83d34843e660c071f2c6ffe7b48f8259.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9090685570cfa05ea9ed092dd7b235f1",
+    "url": "/coding-2-try-phaser-Sethpr/finalassets/Images/Boss.png"
+  },
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "/coding-2-try-phaser-Sethpr/finalassets/Images/background.png"
+  },
+  {
+    "revision": "0741cebdde4f0657781b19ff503a999e",
+    "url": "/coding-2-try-phaser-Sethpr/finalassets/Images/ball.png"
+  },
+  {
+    "revision": "6be3462bcaa4a183b678800bb66d176c",
+    "url": "/coding-2-try-phaser-Sethpr/finalassets/Images/beam.png"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-Sethpr/finalassets/Images/explosion.png"
+  },
+  {
+    "revision": "05427110fd2c2de1d4738b31496a53c9",
+    "url": "/coding-2-try-phaser-Sethpr/finalassets/Images/power-up.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-Sethpr/finalassets/Images/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-Sethpr/finalassets/Images/ship2.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-Sethpr/finalassets/Images/ship3.png"
+  },
+  {
+    "revision": "584229b5d4b8bf3db3f35202881f0000",
+    "url": "/coding-2-try-phaser-Sethpr/finalassets/citations.md"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/coding-2-try-phaser-Sethpr/finalassets/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/coding-2-try-phaser-Sethpr/finalassets/font.xml"
+  },
+  {
+    "revision": "853cce00ba94fd9ec6c9987763776763",
+    "url": "/coding-2-try-phaser-Sethpr/finalfavicon.ico"
+  },
+  {
+    "revision": "19ae082a0ad4abc4ed48",
+    "url": "/coding-2-try-phaser-Sethpr/finalgen.main.7f3abd0f0361a9a83735.bundle.js"
+  },
+  {
+    "revision": "324f31435cec3151a5ed",
+    "url": "/coding-2-try-phaser-Sethpr/finalgen.vendors.a1a6c04959c089cbf1e5.bundle.js"
+  },
+  {
+    "revision": "91b76f15eccd65fedca47a5a7f92cfb9",
+    "url": "/coding-2-try-phaser-Sethpr/finalindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-Sethpr/finalmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-Sethpr/finalsw.js"
+  }
+]);
